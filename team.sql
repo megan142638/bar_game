2fa0d144eba97a3595532911901e0546a3fa21b8
Megan Tsai <megan142638@gmail.com>
2018-12-22 17:08:22 +0800

    Create team.sql

---
+++ team.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2018 年 12 月 22 日 16:19
+-- 伺服器版本: 10.1.32-MariaDB
+-- PHP 版本： 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `team`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `content`
+--
+
+CREATE TABLE `content` (
+  `roomNo` int(10) NOT NULL,
+  `player` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `role` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 資料表的匯出資料 `content`
+--
+
+INSERT INTO `content` (`roomNo`, `player`, `role`) VALUES
+(1, 'adm', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `list`
+--
+
+CREATE TABLE `list` (
+  `roomNo` int(11) NOT NULL,
+  `name` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `leaderID` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `count` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 資料表的匯出資料 `list`
+--
+
+INSERT INTO `list` (`roomNo`, `name`, `leaderID`, `count`) VALUES
+(1, 'test', 'adm', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `user`
+--
+
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL,
+  `loginID` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `user`
+--
+
+INSERT INTO `user` (`uid`, `loginID`, `password`) VALUES
+(1, 'test', '123'),
+(2, 'adm', '123'),
+(3, 'test1', '123');
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `list`
+--
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`roomNo`);
+
+--
+-- 資料表索引 `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `list`
+--
+ALTER TABLE `list`
+  MODIFY `roomNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表 AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
