aaaf90a82346ce5301be4e3909f9a332be86806f
鎮霖 <43329631+n50314@users.noreply.github.com>
2019-01-04 20:19:20 +0800

    Add files via upload

--- register.php
+++ register.php
@@ -9,7 +9,6 @@ if ($loginID) {
 	$stmt = mysqli_prepare($db, $sql);
 	mysqli_stmt_bind_param($stmt, "ss", $loginID, $password);
 	mysqli_stmt_execute($stmt);
-	echo "已註冊!!";
 
 	echo "<script>alert('警告：註冊成功!將在確認之後跳回登入頁面'); location.href = 'http://localhost/beer_game-master/login.html';</script>";
 }
