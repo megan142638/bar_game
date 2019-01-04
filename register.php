149bcaf91c6a3010893d5bac66f9b483ed34c7c6
royaldin <huangandyamy@gmail.com>
2019-01-04 19:46:10 +0800

    Update register.php

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ $loginID = $_POST['uid'];
 $password = $_POST['pwd'];
 
 if ($loginID) {
-	$sql = "insert into user (loginID, password) values ('$loginID','$password')";
+	$sql = "insert into user (loginID, password) values (?,?)";
 	$stmt = mysqli_prepare($db, $sql);
 	mysqli_stmt_bind_param($stmt, "ss", $loginID, $password);
 	mysqli_stmt_execute($stmt);
