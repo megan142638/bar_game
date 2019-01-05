e2b83a1616626a5e77de7ced877d66e56f881370
royaldin <huangandyamy@gmail.com>
2019-01-05 14:33:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,10 @@
 <?php
 require("userModel.php");
-
-$userName = $_POST['id'];//從login表單抓取使用者輸入的帳密連結db資訊
-$passWord = $_POST['pwd'];
-if (login($userName, $passWord)==1) {
-    if(getRole()==Admin){
-        header("Location: 01.php" );//管理者
-    }
-} else{
+$loginID = $_POST['id'];
+$pwd = $_POST['pwd'];
+if (login($loginID, $pwd)==1) {
+    header("Location: teamlist.php");
+} else {
     header("Location: login.html");
 }
 ?>
