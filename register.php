49548426109178b34338ef5b7bd00fb42e9d1262
royaldin <huangandyamy@gmail.com>
2019-01-05 23:52:09 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,17 +1,21 @@
 <?php
 require("dbconfig.php");
-
 $loginID = $_POST['uid'];
 $password = $_POST['pwd'];
 
-if ($loginID) {
-	$sql = "insert into user (loginID, password) values (?,?)";
-	$stmt = mysqli_prepare($db, $sql);
-	mysqli_stmt_bind_param($stmt, "ss", $loginID, $password);
-	mysqli_stmt_execute($stmt);
 
-	echo "<script>alert('註冊成功!將在確認之後跳回登入頁面'); location.href = 'http://localhost/beer_game-master/login.html';</script>";
+$sqlloginID = "SELECT loginID FROM user where loginID = '".$loginID."'";
+if ($result = mysqli_query($db,$sqlloginID)) {
+        if ($row=mysqli_fetch_array($result)) {
+                echo "<script>alert('ID已存在!將在確認之後跳回註冊頁面'); location.href = 'http://localhost/beer_game-master/register.html';</script>";
+        } else {
+                $sql = "insert into user (loginID, password) values (?,?)";
+                $stmt = mysqli_prepare($db, $sql);
+                mysqli_stmt_bind_param($stmt, "ss", $loginID, $password);
+                mysqli_stmt_execute($stmt);
+                echo "<script>alert('註冊成功!將在確認之後跳回登入頁面'); location.href = 'http://localhost/beer_game-master/login.html';</script>";
+        }
 }
 
-
 ?>
+
