5199a0325b8d21d4024344640ecc8eddcd2e523c
Royal Din <huangandyamy@gmail.com>
2019-01-08 17:30:05 +0800

    Update register.php

--- register.php
+++ register.php
@@ -2,6 +2,20 @@
 require("dbconfig.php");
 $loginID = $_POST['uid'];
 $password = $_POST['pwd'];
+$sqlloginID = "SELECT loginID FROM user where loginID = '".$loginID."'";
+
+if ($result = mysqli_query($db,$sqlloginID)) {
+        if ($row=mysqli_fetch_array($result)) {
+                echo "<script>alert('ID已存在!將在確認之後跳回註冊頁面'); location.href = 'http://localhost/beer_game-master/register.html';</script>";
+        } else {
+                $sql = "INSERT into user (loginID, password) values (?,?)";
+                $stmt = mysqli_prepare($db, $sql);
+                mysqli_stmt_bind_param($stmt, "ss", $loginID, $password);
+                mysqli_stmt_execute($stmt);
+                print_r($stmt);
+                echo "<script>alert('註冊成功!將在確認之後跳回登入頁面'); location.href = 'http://localhost/beer_game-master/login.html';</script>";
+        }
+}
 
 $arrTypes =array ("image/gif", "image/png", "image/jpeg", "image/pjpeg");
 if ($_FILES['pic']['error']==0) { // Error?
@@ -24,16 +38,6 @@ if ($_FILES['pic']['error']==0) { // Error?
 } else {
    echo "Error: " . $_FILES["pic"]["error"] . "<br />";
 }
-/*
-if ($result = mysqli_query($db,$sqlloginID)) {
-        if ($row=mysqli_fetch_array($result)) {
-                echo "<script>alert('ID已存在!將在確認之後跳回註冊頁面'); location.href = 'http://localhost/beer_game-master/register.html';</script>";
-        } else {
-                $sql = "insert into user (loginID, password) values (?,?)";
-                $stmt = mysqli_prepare($db, $sql);
-                mysqli_stmt_bind_param($stmt, "ss", $loginID, $password);
-                mysqli_stmt_execute($stmt);
-                echo "<script>alert('註冊成功!將在確認之後跳回登入頁面'); location.href = 'http://localhost/beer_game-master/login.html';</script>";
-        }
-} */
+
+
 ?>
