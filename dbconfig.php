12c11439a6da328c73981c1fa069bfd673f40ced
Anakin Chan <44217650+aaanakin@users.noreply.github.com>
2019-01-08 18:18:45 +0800

    change dbName&pass

--- dbconfig.php
+++ dbconfig.php
@@ -3,7 +3,8 @@ session_start();
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbName = 'beer_game';
+$dbName = 'beerteam';
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 mysqli_query($db,"SET NAMES utf8"); //選擇編碼
+
 ?>
