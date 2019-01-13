4f436db52f44cd07400224624e20ab02ce0ded01
Royal Din <huangandyamy@gmail.com>
2019-01-13 17:17:21 +0800

    Update teamlist.php

--- team/teamlist.php
+++ team/teamlist.php
@@ -58,6 +58,7 @@ $per=getPermission();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>隊伍列表</title>
 <meta http-equiv="refresh" content="3" />
@@ -67,13 +68,13 @@ $per=getPermission();
 
 <p>Team List !!</p>
 <hr />
-<table width="600" border="1" class="">
-  <tr>
+<table width="600" border="1" class="table table-hover">
+  <tr class = "table-info">
     <td>id</td>
     <td>隊伍名稱</td>
     <td>隊長</td>
     <td>人數</td>
-<td>選項</td>
+    <td>選項</td>
   </tr>
 <?php
 while ($rs = mysqli_fetch_assoc($result)) {
