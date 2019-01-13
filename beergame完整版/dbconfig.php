f02e09532244d0953e9e51412f9cef3531bf6de1
Royal Din <huangandyamy@gmail.com>
2019-01-13 20:48:55 +0800

    Update dbconfig.php

--- beergame完整版/dbconfig.php
+++ beergame完整版/dbconfig.php
@@ -3,7 +3,7 @@ session_start();
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbName = 'beer_game';
+$dbName = 'team';
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 mysqli_query($db,"SET NAMES utf8"); //選擇編碼
 ?>
