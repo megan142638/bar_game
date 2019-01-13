45e1cdabaed14d67d4a417b2984b1cbb1a8b8fa0
Royal Din <huangandyamy@gmail.com>
2019-01-13 20:06:17 +0800

    Update creatTeam.php

--- team/creatTeam.php
+++ team/creatTeam.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+  <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>創立隊伍</title>
 <link rel="stylesheet" type="text/css" href="main.css">
@@ -11,10 +12,10 @@
 <p>創建隊伍</p>
 <hr />
 <form method="post" action="insert.php">
-<table width="400" border="1" class="">
-  <tr>
+<table width="400" border="1" class="table table-hover">
+  <tr class = "table-primary">
     <td>隊伍名稱</td><td><input type="text" name="title" required/></td></tr>
-  <tr>
+  <tr class = "table-primary">
     <td>選擇角色</td><td>
     <input type="radio" name="role" value="retailer" checked/> 零售商
     <input type="radio" name="role" value="wholesaler" /> 批發商
